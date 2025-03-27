175a4ba9f112a48887e4ed57cca81e2de271ff6e
Naman Pandey <2492004namanpandey@gmail.com>
2025-03-27 18:21:06 +0530

    initial setup with shadcn and clerk

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AiMotoMart",
+  description: "Find your dream Car at AiMotoMart",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} `}>
+          <Header />
+
+          <main className="min-h-screen">{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
